871b44394810cf59f280a97d6058fd11f55249b5
oxyvans <benja.1999.g@hotmail.com>
2022-04-25 09:40:44 -0700

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,19 +12,19 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow_p, *fast_p;
 
-	if (list == NULL)
+	if (!list || !list->next)
 		return (0);
 
-	fast_p = list->next;
-	slow_p = list;
+	fast_p = list->next->next;
+	slow_p = list->next;
 
 	while (slow_p && fast_p && fast_p->next)
 	{
-		if (slow_p->n == fast_p->n)
+		if (slow_p == fast_p)
 			return (1);
 
 		slow_p = slow_p->next;
-		fast_p = fast_p->next;
+		fast_p = fast_p->next->next;
 	}
 
 	return (0);

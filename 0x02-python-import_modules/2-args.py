8d08fe6d7070bafe7322524700aa6361a2dc943a
oxyvans <benja.1999.g@hotmail.com>
2022-04-28 09:06:32 -0700

    02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print("1 argument:")
     else:
-        print(f"{len(sys.argv) - 1} argument:")
+        print(f"{len(sys.argv) - 1} arguments:")
 
     for i in range(1, len(sys.argv)):
         print(f"{i}: {sys.argv[i]}")

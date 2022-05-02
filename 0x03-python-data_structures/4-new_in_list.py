76480c3405cfd30cf8a6c85b8c253e7bd353a421
oxyvans <benja.1999.g@hotmail.com>
2022-05-02 06:49:47 -0700

    04

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return (my_list)
+    cp = my_list.copy()
+    cp[idx] = element
+    return (cp)

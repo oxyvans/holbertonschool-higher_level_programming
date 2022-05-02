2180575661e14e744aa7fdabc38960ae123dceea
oxyvans <benja.1999.g@hotmail.com>
2022-05-02 07:39:12 -0700

    05

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def no_c(my_string):
+    str = ""
+    for c in range(len(my_string)):
+        if my_string[c] != 'c' and my_string[c] != 'C':
+            str = str + my_string[c]
+    return (str)

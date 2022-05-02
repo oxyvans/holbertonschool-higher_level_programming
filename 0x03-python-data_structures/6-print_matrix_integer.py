f52467472a4662b87fe95f7bb3a2a4478120ded5
oxyvans <benja.1999.g@hotmail.com>
2022-05-02 08:58:24 -0700

    06

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if c == len(matrix[0]) - 1:
+                print(f"{matrix[r][c]}",end='')
+            else:
+		print(f"{matrix[r][c]}",end=' ')
+        print("")

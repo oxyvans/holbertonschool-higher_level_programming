214e5bb9aaec604aa086432d9a68279532a3ba3f
oxyvans <benja.1999.g@hotmail.com>
2022-05-02 10:19:10 -0700

    09

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max = 0
+    if len(my_list) == 0:
+        return (None)
+    for n in range(len(my_list)):
+        if max < my_list[n]:
+            max = my_list[n]
+    return (max)

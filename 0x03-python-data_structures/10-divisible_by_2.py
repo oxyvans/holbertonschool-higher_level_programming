6f9534ea43a9c10bd9c252a26cc1da62f9f64b1a
oxyvans <benja.1999.g@hotmail.com>
2022-05-02 10:33:59 -0700

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    res = []
+    for n in my_list:
+        if n % 2 == 0:
+            res.append(True)
+        else:
+            res.append(False)
+    return (res)

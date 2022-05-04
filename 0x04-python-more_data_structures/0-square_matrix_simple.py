c97768ed2a1058c603373ba0e0866f1ff6bac8e9
oxyvans <benja.1999.g@hotmail.com>
2022-05-04 09:49:01 -0700

    0 and 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    res = matrix.copy()
+    for i in range(len(matrix)):
+        res[i] = list(map(lambda x: x*x, matrix[i]))
+    return(res)
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    res = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] is search:
+            res[i] = replace
+    return(res)

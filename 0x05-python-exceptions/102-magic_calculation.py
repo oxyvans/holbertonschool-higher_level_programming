90e9456013ee1b8aaffa24418f2e8a00d3745a06
oxyvans <benja.1999.g@hotmail.com>
2022-05-16 11:14:12 -0700

    102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += a ** b / i
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, IndexError):
             result = b + a
             break
     return result

babc534e527aa80219b7ac4536233bd21c7fa536
oxyvans <benja.1999.g@hotmail.com>
2022-05-18 11:54:45 -0700

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,6 +75,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """biggest rectangle"""
         if type(rect_1) is not Rectangle:

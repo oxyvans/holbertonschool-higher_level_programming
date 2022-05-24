67a700c3a744f46ca88776f771259a6e78be52a8
oxyvans <benja.1999.g@hotmail.com>
2022-05-24 08:06:41 -0700

    fix task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,14 +4,17 @@ def text_indentation(text):
     """text indentation"""
     if type(text) is not str:
         raise TypeError("text must be a string")
-    c = 0
-    while(c < len(text)):
-        if text[c] == '.' or text[c] == '?' or text[c] == ':':
-            print(text[c])
-            print()
-            c += 1
-            while(text[c] == ' '):
-                c += 1
-        else:
-            print(text[c],end="")
-            c += 1
+    flag = False
+    for c in text:
+        if not flag:
+            if c == ' ':
+                continue
+            else:
+                flag = True
+        if flag:
+            if c == '.' or c == '?' or c == ':':
+                print(c)
+                print()
+                flag = False
+            else:
+                print(c, end="")

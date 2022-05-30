81bfa6a7285d6a299309d856f45592a471be8955
oxyvans <benja.1999.g@hotmail.com>
2022-05-30 16:51:01 -0700

    fix betty

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,7 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ validator """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
@@ -21,7 +21,7 @@ class Rectangle(BaseGeometry):
     """ Rectangle """
     def __init__(self, width, height):
         """init"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height

b82b3514fd1b0ede022119bc0add45bf32cd0d00
oxyvans <benja.1999.g@hotmail.com>
2022-05-30 11:39:05 -0700

    add task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+
+
+class BaseGeometry:
+    """ Task 8 """
+
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validator """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ area """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """ print """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """ square """
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size

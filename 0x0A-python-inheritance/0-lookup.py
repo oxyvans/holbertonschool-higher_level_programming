d242efae3b2cb8dc7be508e349a40bc0e631ef78
oxyvans <benja.1999.g@hotmail.com>
2022-05-30 07:26:02 -0700

    add tasks 0 ans 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+
+def lookup(obj):
+    """ task 0 """
+    return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+class MyList(list):
+    """ task 1 """
+
+    def print_sorted(self):
+        """ sortrd """
+        print(sorted(self))

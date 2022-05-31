e3527cc454da3a283f230a2a520b5c10a462ea07
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 09:18:59 -0700

    add task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" Input/Output """
+
+
+class Student:
+    """ class """
+
+    def __init__(self, first_name, last_name, age):
+        """ init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ json """
+        return self.__dict__

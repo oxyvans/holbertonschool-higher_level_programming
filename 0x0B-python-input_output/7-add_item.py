903e6a8c4fb4bcad6fb1701de587d38a2d571abe
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 08:49:12 -0700

    add task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" Input/Output """
+
+
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+
+try:
+    l = load_from_json_file(filename)
+except FileNotFoundError:
+    l = []
+
+res = l + argv[1:]
+save_to_json_file(res, filename)

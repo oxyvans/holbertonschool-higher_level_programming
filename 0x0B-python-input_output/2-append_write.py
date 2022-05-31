7b5526fc72f893b12d3ec409bf21df8396243cd4
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 06:02:47 -0700

    add task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Input/Output """
+
+
+def append_write(filename="", text=""):
+    """ task 2 """
+    with open('filename', 'a', encoding="utf-8") as f:
+        a = f.write(text)
+    return a

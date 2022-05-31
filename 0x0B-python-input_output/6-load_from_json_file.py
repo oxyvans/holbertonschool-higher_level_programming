7d0483754f06507af42639250a58fec8fb12a057
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 07:08:45 -0700

    add task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Input/Output """
+
+
+import json
+
+def load_from_json_file(filename):
+    """ task 6 """
+    with open(filename, 'r', encoding="utf-8") as f:
+         x = json.load(f)
+    return x

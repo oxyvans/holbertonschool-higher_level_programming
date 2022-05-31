c8e5a04667e0e44e6a9195a205a093ab9fd1e3a2
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 05:55:57 -0700

    add task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Input/Output """
+
+
+def write_file(filename="", text=""):
+    """ task 1 """
+    with open('filename', 'w', encoding="utf-8") as f:
+        w = f.write(text)
+    return w

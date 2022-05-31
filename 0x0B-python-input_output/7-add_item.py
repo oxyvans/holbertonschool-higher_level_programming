8a71635dcdae3a296b5d9d8d83f0637815ceccd7
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 08:56:44 -0700

    add task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """ Input/Output """
 
 
-import sys
+from sys import *
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file

6af4cd096ab687004f2469609737b0abbde5c7b9
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 05:41:40 -0700

    add task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Input/Output """
+
+
+def read_file(filename=""):
+    """ task 0 """
+    with open('filename', 'r', encoding="utf-8") as f:
+        r = f.read()
+    print(r)

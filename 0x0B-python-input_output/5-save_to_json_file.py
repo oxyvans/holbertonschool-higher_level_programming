287b7bfc474b88cd0667aa48ff77bb8b6c380af0
oxyvans <benja.1999.g@hotmail.com>
2022-05-31 06:43:12 -0700

    add task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Input/Output """
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """task 5 """
+    with open(filename, 'w', encoding="utf-8") as f:
+        x = json.dumps(my_obj)
+        f.write(x)

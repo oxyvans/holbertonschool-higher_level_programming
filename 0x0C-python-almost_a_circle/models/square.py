24aa0b80af62e5432c062fc1ff5224307f60b67d
oxyvans <benja.1999.g@hotmail.com>
2022-06-03 09:31:00 -0700

    add task 9

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Almost a circle """
+
+
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ init method """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ info """
+        return ("[Square] ({}) {}/{} - {}".format
+                (self.id, self.x, self.y, self.width))

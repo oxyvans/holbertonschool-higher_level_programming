9d8fe131b33de946be4d1790258c048bf876fbd5
oxyvans <benja.1999.g@hotmail.com>
2022-06-06 07:13:52 -0700

    add task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,15 @@ class Base:
         if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ salve """
+        lis = []
+        for i in list_objs:
+            lis.append(cls.to_dictionary(i))
+        res = cls.to_json_string(lis)
+        filename = ("{}.json".format(cls.__name__))
+
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(res)

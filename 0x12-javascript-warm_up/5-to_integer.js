ec2bc64424f988024aa76fea795d44121659a1de
oxyvans <benja.1999.g@hotmail.com>
2022-08-01 09:41:05 -0700

    add task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,8 @@
 const arg = process.argv;
 const n = parseInt(arg[2]);
 
-if (isNaN(n))
-	console.log('Not a number');
-else
-	console.log('My number:', n);
+if (isNaN(n)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', n);
+}

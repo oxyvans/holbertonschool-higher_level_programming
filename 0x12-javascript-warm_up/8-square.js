1d78a2d8c12e32aec2115eaf8da5bb7350a4714b
oxyvans <benja.1999.g@hotmail.com>
2022-08-01 10:57:02 -0700

    add task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const time = parseInt(process.argv[2]);
 
 if (isNaN(time)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < time; i++) {
     console.log('X'.repeat(time));

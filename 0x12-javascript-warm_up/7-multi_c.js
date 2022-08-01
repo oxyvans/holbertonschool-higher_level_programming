e4da7143e82745727eeda62d120c64f98b199418
oxyvans <benja.1999.g@hotmail.com>
2022-08-01 10:35:51 -0700

    add task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const time = parseInt(process.argv[2]);
+
+if (isNaN(time)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < time; i++) {
+    console.log('C is fun');
+  }
+}

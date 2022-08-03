b0fa6e47016159703e541836206016981cd29650
oxyvans <benja.1999.g@hotmail.com>
2022-08-02 18:00:41 -0700

    add task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+const res = {};
+Object.entries(dict).forEach(key => {
+  if (res[key[1]]) {
+    res[key[1]].push(key[0]);
+  } else {
+    res[key[1]] = [key[0]];
+  }
+});

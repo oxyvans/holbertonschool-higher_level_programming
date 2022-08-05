f9da7d0fcd215ba1b247d9928c4047f76f299c1e
oxyvans <benja.1999.g@hotmail.com>
2022-08-05 16:55:11 +0000

    add task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,11 +7,16 @@ if __name__ == "__main__":
     USER = argv[1]
     PASS = argv[2]
     DB = argv[3]
+    STATE = argv[4]
 
 try:
-    db = MySQL.connect(host=HOST, user=USER, passwd=PASS, db=DB)
+    db = MySQLdb.connect(host=HOST, user=USER, passwd=PASS, db=DB)
     cur = db.cursor()
-    cur.execute("")
+    cur.execute("SELECT cities.name FROM cities\
+                    LEFT JOIN states\
+                        ON cities.state_id = states.id\
+                            WHERE states.name=%s\
+                                ORDER BY cities.id ASC", (STATE,))
     rows = cur.fetchall()
     for row in rows:
         print(row)

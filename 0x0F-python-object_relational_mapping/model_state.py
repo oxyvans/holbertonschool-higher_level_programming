a667faa82de03ab3209ce9dcb4aa12e0b71e7ad3
oxyvans <benja.1999.g@hotmail.com>
2022-08-07 07:35:09 -0700

    add task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""sqlalchemy"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-base = declarative_base()
+Base = declarative_base()
 
-class State(base):
-    """ states """
+
+class State(Base):
+    """state"""
     __tablename__ = 'states'
-     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
-     name = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)

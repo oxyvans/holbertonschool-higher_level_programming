67e62d3cffae0ac3038ff2c6ce3f6f9431babed5
oxyvans <benja.1999.g@hotmail.com>
2022-08-07 07:48:13 -0700

    add task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,15 +3,12 @@
 from sys import argv
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 
-USER = argv[1]
-PASS = argv[2]
-DB = argv[3]
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(USER, PASS, DB))
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker()
     Session.configure(bind=engine)

80bb03eca411a4478428d5e79c725b83f51f863e
oxyvans <benja.1999.g@hotmail.com>
2022-08-19 10:22:55 -0700

    add task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" task 1"""
+
+from sys import argv
+from urllib import request
+
+
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as response:
+        print(response.getheader("X-Request-Id"))

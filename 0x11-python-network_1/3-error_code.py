7253a8bc5e53d6e2ffbcfb45648b7e10242db505
oxyvans <benja.1999.g@hotmail.com>
2022-08-19 10:58:32 -0700

    add task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" task 3 """
+
+from sys import argv
+from urllib import request, error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))

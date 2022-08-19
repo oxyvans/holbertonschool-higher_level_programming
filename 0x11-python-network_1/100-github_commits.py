ec3222ec3c1d95c0a67f67f651d3812ecd08e3fa
oxyvans <benja.1999.g@hotmail.com>
2022-08-19 12:40:25 -0700

    add task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,6 +8,6 @@ from requests import get
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     r = get(url)
-    for comit in r.json():
+    for comit in r.json()[0:10]:
         print(comit.get('sha'), end=': ')
         print(comit.get('commit').get('author').get('name'))

643111d623abed3af0614f51f8819bb35c2682bc
oxyvans <benja.1999.g@hotmail.com>
2022-09-12 14:47:12 -0700

    add task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//task 2
+//
+const axios = require('axios');
+axios.get(process.argv[2]).then(function (response) {
+  console.log('code: ' + response.status);
+}).catch(function (error) {
+  console.log('code: ' + error.response.status);
+});

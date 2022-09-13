56ae423951c4ca89ca53d2ba3dfb488ce59a75d1
benjamin gutierrez <81387977+oxyvans@users.noreply.github.com>
2022-09-12 21:37:04 -0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,1 @@
-#!/usr/bin/node
-// task 4
 
-const axios = require('axios');
-const url = process.argv[2];
-const p18 = 'https://swapi-api.hbtn.io/api/people/18/';
-let res = 0;
-
-axios.get(url).then(function (response) {
-  const films = response.data.results;
-  for (let n = 0; n < films.length; n++) {
-    if (films[n].characters.includes(p18)) {
-      res = res + 1;
-    }
-  }
-  console.log(res);
-}).catch(function (error) {
-  console.log(error);
-});
